0c24022633dab60720f6bbff7c7dd72810e975b1
Shreyas <shreyashirday@gmail.com>
2014-11-22 18:15:51 -0500

    traversal 1

--- agent.cpp
+++ agent.cpp
@@ -3,14 +3,47 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "decepticon/decepticon.hpp"
 
+
 using namespace cv;
+void traverse(Decepticon *dec);
+bool matchImage(Decepticon *dec);
+bool initial = false;
 
 int main() {
   Decepticon d;
-  //test
-  //////////////////////////
-  // PLACE YOUR CODE HERE //
-  //////////////////////////
   
+  
+
   return 0;
+}
+
+
+
+void traverse(Decepticon& dec){
+  int distanceInCm = dec.get_sonar();
+  int distanceInMeters = distanceInCm/100;
+  double metersToFeet = distanceInMeters * 3.28;
+  double metersToInches = distanceInMeters * 39.37;
+  
+  if(metersToFeet < 2.1){
+          dec.set_left(150)
+    
+
+  }
+
+
+
+}
+
+
+double getFeet(Decepticon& dec){
+
+return ((dec.get_sonar()/100) * 3.28);
+
+}
+
+double getInches(Decepticon& dec){
+
+return ((dec.get_sonar()/100) * 39.37);
+
 }
